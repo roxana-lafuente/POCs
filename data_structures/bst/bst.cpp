96b8fdc34d75b08b13b87904928f188d685bcb95
Roxana <roxannitha@gmail.com>
2016-05-22 20:10:52 -0300

    Recursive C++ BST Working version

--- data_structures/bst/bst.cpp
+++ data_structures/bst/bst.cpp
@@ -5,75 +5,211 @@ Date: 22-05-2016
 */
 
 #include <iostream>
+#include <stack>
+#include <list>
 
 using namespace std;
 
+/* Enumeration to decide which implementation to use */
+enum algorithmType
+{
+    RECURSIVE,
+    ITERATIVE
+};
+
+/* BST node */
 template <typename T>
 struct node
 {
-    T value;
-    node<T>* left;
-    node<T>* right;
+    T value; // value of the node
+    node<T>* left; // left tree
+    node<T>* right; // right tree
+    algorithmType mode; // iterative or recursive
 
-    node(T v, node<T>* l, node<T>* r) : value(v), left(l), right(r) {}
+    node(T v, node<T>* l, node<T>* r, algorithmType m) : value(v),
+                                                         left(l),
+                                                         right(r),
+                                                         mode(m) {}
     ~node()
     {
-        // for every node in the BST, delete it
+        if (left)
+            delete left;
+        if (right)
+            delete right;
     }
+
+    node<T> getGreatestNode();
+    node<T> recursiveGetGreatestNode(node<T>* n);
+    node<T> iterativeGetGreatestNode(node<T>* n);
+
     void insertNode(T v);
     void recursiveInsertNode(T v, node<T>* n);
+    void iterativeInsertNode(T v);
+
     void deleteNode(T v);
     void recursiveDeleteNode(T v, node<T>* n);
+    void iterativeDeleteNode(T v);
+
     node<T> search(T v);
+    void recursiveSearch(T v, node<T>* n);
+    void iterativeSearch(T v);
+
     void print();
+    void recursivePrint(node<T>* n);
+    void iterativePrint();
 
 };
+
+/* Returns the greatest node in the BST */
+template <typename T>
+node<T> node<T>::getGreatestNode()
+{
+    switch (mode)
+    {
+        default:
+        case(RECURSIVE):
+        {
+            return recursiveGetGreatestNode(this);
+        }
+        case(ITERATIVE):
+        {
+            return iterativeGetGreatestNode(this);
+        }
+    }
+}
+
+/* Recursively returns the greatest node in the BST */
+template <typename T>
+node<T> node<T>::recursiveGetGreatestNode(node<T>* n)
+{
+    if(n->right != NULL)
+    {
+        return recursiveGetGreatestNode(n->right);
+    }
+    else
+    {
+        return n->value;
+    }
+}
+
+/* Iteratively returns the greatest node in the BST */
+template <typename T>
+node<T> node<T>::iterativeGetGreatestNode(node<T>* n)
+{
+    node<T> it = n;
+    while(it->right != NULL)
+    {
+        it = it->right;
+    }
+    return it->value;
+}
 
 /* Inserts a new node to the BST */
 template <typename T>
 void node<T>::insertNode(T v)
 {
-    recursiveInsertNode(v, this);
+    switch (mode)
+    {
+        default:
+        case(RECURSIVE):
+        {
+            recursiveInsertNode(v, this);
+        }
+        case(ITERATIVE):
+        {
+            iterativeInsertNode(v);
+        }
+    }
 }
 
-/* Recursively insert a node */
+/* Recursively inserts a node */
 template <typename T>
 void node<T>::recursiveInsertNode(T v, node<T>* n)
 {
-    if(n->left==NULL && n->right==NULL)
+    if(v>value)
     {
-        // Add new node to the left tree.
-        left = new node<T>(v, NULL, NULL);
+        if(n->right!=NULL)
+        {
+            recursiveInsertNode(v, right);
+        }
+        else
+        {
+            n->right = new node<T>(v, NULL, NULL, mode);
+            cout << "Alloc memory for " << v << endl;
+        }
     }
-    else if(v>value)
+    else if(v<value)
     {
-        // Insert node to the right tree.
-        recursiveInsertNode(v, right);
+        if (n->left!=NULL)
+        {
+            recursiveInsertNode(v, left);
+        }
+        else
+        {
+            n->left = new node<T>(v, NULL, NULL, mode);
+            cout << "Alloc memory for " << v << endl;
+        }
     }
-    else
-    {
-        // Insert node to the left tree.
-        recursiveInsertNode(v, left);
-    }
+}
+
+/* Iteratively inserts a node */
+template <typename T>
+void node<T>::iterativeInsertNode(T v)
+{
+    // TODO
 }
 
 /* Deletes a specific node from the BST */
 template <typename T>
 void node<T>::deleteNode(T v)
 {
-    recursiveDeleteNode(v, this);
+    switch (mode)
+    {
+        default:
+        case(RECURSIVE):
+        {
+            recursiveDeleteNode(v, this);
+        }
+        case(ITERATIVE):
+        {
+            iterativeDeleteNode(v, this);
+        }
+    }
 }
 
-/* Recursively delete a node */
+/* Recursively deletes a node */
 template <typename T>
 void node<T>::recursiveDeleteNode(T v, node<T>* n)
 {
     if(n->value == v)
     {
+        node<T>* toDelete;
         if(n->left==NULL && n->right==NULL)
         {
-            //left = new node<T>(v, NULL, NULL);
+            toDelete = n;
         }
+        else if(n->left==NULL && n->right!=NULL)
+        {
+            toDelete = n->right;
+            n->value = toDelete->value;
+            n->right = toDelete->right;
+            n->left = toDelete->left;
+        }
+        else if(n->left!=NULL && n->right==NULL)
+        {
+            toDelete = n->left;
+            n->value = toDelete->value;
+            n->right = toDelete->right;
+            n->left = toDelete->left;
+        }
+        else
+        {
+            toDelete = getGreatestNode(n->left);
+            n->value = toDelete->value;
+            n->right = toDelete->right;
+            n->left = toDelete->left;
+        }
+        delete toDelete;
     }
     else if(v>value)
     {
@@ -87,9 +223,52 @@ void node<T>::recursiveDeleteNode(T v, node<T>* n)
     }
 }
 
+/* Iteratively deletes a node */
+template <typename T>
+void node<T>::iterativeDeleteNode(T v)
+{
+    // TODO
+}
+
 /* Searches for a specific node and returns it */
 template <typename T>
 node<T> node<T>::search(T v)
+{
+    switch (mode)
+    {
+        default:
+        case(RECURSIVE):
+        {
+            recursiveSearch(v, this);
+        }
+        case(ITERATIVE):
+        {
+            iterativeSearch(v, this);
+        }
+    }
+}
+
+/* Recursively searches for a specific node */
+template <typename T>
+void node<T>::recursiveSearch(T v, node<T>* n)
+{
+    if(n->value == v)
+    {
+        return n;
+    }
+    else if(v>value)
+    {
+        recursiveSearch(v, right);
+    }
+    else
+    {
+        recursiveSearch(v, left);
+    }
+}
+
+/* Iteratively searches for a specific node */
+template <typename T>
+void node<T>::iterativeSearch(T v)
 {
     // TODO
 }
@@ -98,13 +277,67 @@ node<T> node<T>::search(T v)
 template <typename T>
 void node<T>::print()
 {
+    switch (mode)
+    {
+        default:
+        case(RECURSIVE):
+        {
+            recursivePrint(this);
+        }
+        case(ITERATIVE):
+        {
+            iterativePrint();
+        }
+    }
+    cout << endl;
+}
+
+/* Recursively prints a BST */
+template <typename T>
+void node<T>::recursivePrint(node<T>* n)
+{
+    if(n != NULL)
+    {
+        cout << "( ";
+        if (n->left != NULL)
+        {
+            recursivePrint(n->left);
+        }
+        else
+        {
+            cout << " LEAF ";
+        }
+        cout << n->value;
+        if(n->right != NULL)
+        {
+            recursivePrint(n->right);
+        }
+        else
+        {
+            cout << " LEAF ";
+        }
+        cout << " )";
+    }
+}
+
+/* Iteratively prints a BST */
+template <typename T>
+void node<T>::iterativePrint()
+{
     // TODO
 }
 
 int
 main(void)
 {
-    node<int> root(2, NULL, NULL);
-    root.insertNode(3);
+    node<int> root(12, NULL, NULL, RECURSIVE);
+    list<int> nodesToInsert = {10, 9};//, 11, 30, 25, 40, 24, 26, 35, 41};
+    list<int>::iterator it = nodesToInsert.begin();
+    while(it != nodesToInsert.end())
+    {
+        root.insertNode(*it);
+        it++;
+    }
+    root.print();
     return 0;
 }
