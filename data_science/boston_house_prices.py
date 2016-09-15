611f26c1c614a1ef0143ae62f18b53503bf0a9ef
zxySp <roxannitha@gmail.com>
2016-09-15 16:34:28 -0300

    Added log transformation to linear model (boston house dataset)

--- data_science/boston_house_prices.py
+++ data_science/boston_house_prices.py
@@ -1,8 +1,9 @@
 from sklearn.datasets import load_boston
 import matplotlib.pyplot as plt
+from math import exp, log
 import statsmodels.api as sm
 import pandas as pd
-import numpy
+import numpy as np
 
 """
 DATA SET INFO:
@@ -36,12 +37,12 @@ boston = pd.DataFrame(dataset.data, columns=dataset.feature_names)
 boston['MEDV'] = dataset.target
 
 # Analise data
-print boston.describe()
-print "\n" * 10
-print boston.corr(method='pearson')
-print "\n" * 10
-corr_with_target = boston.corr(method='pearson').ix[-1][:-1]
-print corr_with_target[abs(corr_with_target).argsort()[::-1]]
+# print boston.describe()
+# print "\n" * 10
+# print boston.corr(method='pearson')
+# print "\n" * 10
+# corr_with_target = boston.corr(method='pearson').ix[-1][:-1]
+# print corr_with_target[abs(corr_with_target).argsort()[::-1]]
 
 # Predict - Ordinary Least Squares (OLS)
 X = sm.add_constant(boston['LSTAT'])
@@ -50,15 +51,29 @@ model = sm.OLS(y, X)
 theta = model.fit()
 theta = theta.params
 
+# Predict - Ordinary Least Squares (OLS) + Log Transformation.
+log_X = sm.add_constant(boston['LSTAT'])
+log_y = np.log(dataset.target)
+log_model = sm.OLS(log_y, log_X)
+log_theta = log_model.fit()
+log_theta = log_theta.params
+
 # Plot data set.
 fig, ax = plt.subplots(figsize=(12,8))
 
 # Linear model.
-x = numpy.linspace(boston.min()['LSTAT'], boston.max()['LSTAT'], N)
+x = np.linspace(boston.min()['LSTAT'], boston.max()['LSTAT'], N)
 f = theta[0] + (theta[1] * x)
 
+# Linear model + Log Transformation.
+log_x = np.linspace(boston.min()['LSTAT'], boston.max()['LSTAT']+16, N)
+log_f = np.exp(log_theta[0] + (log_theta[1] * log_x))
+
 # Plot linear model.
-ax.plot(x, f, 'r', label='Prediction', color='Black')
+ax.plot(x, f, 'r', label='OLS Prediction', color='Black')
+
+# Plot linear model + Log Transformation.
+ax.plot(log_x, log_f, 'r', label='OLS Prediction + Log Transformation', color='Red')
 
 # Scatter
 ax.scatter(boston['MEDV'], boston['LSTAT'], label='Dataset', color='Cyan')
