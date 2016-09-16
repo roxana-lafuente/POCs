165ab404414c16fd5dee6d9b665be43ef0b63c9d
Roxana <roxannitha@gmail.com>
2016-09-16 19:17:35 -0300

    Adding multivariate OLS model to Boston house prices dataset regression

--- data_science/boston_house_prices.py
+++ data_science/boston_house_prices.py
@@ -38,12 +38,12 @@ boston = pd.DataFrame(dataset.data, columns=dataset.feature_names)
 boston['MEDV'] = dataset.target
 
 # Analise data
-# print boston.describe()
-# print "\n" * 10
-# print boston.corr(method='pearson')
-# print "\n" * 10
-# corr_with_target = boston.corr(method='pearson').ix[-1][:-1]
-# print corr_with_target[abs(corr_with_target).argsort()[::-1]]
+print boston.describe()
+print "\n" * 10
+print boston.corr(method='pearson')
+print "\n" * 10
+corr_with_target = boston.corr(method='pearson').ix[-1][:-1]
+print corr_with_target[abs(corr_with_target).argsort()[::-1]]
 
 # Predict - Ordinary Least Squares (OLS)
 X = sm.add_constant(boston['LSTAT'])
@@ -51,6 +51,16 @@ y = dataset.target
 model = sm.OLS(y, X)
 theta = model.fit()
 theta = theta.params
+
+# Predict - Ordinary Least Squares (OLS) - Multivariate: LSTAT + RM
+X = pd.DataFrame(boston['LSTAT'])
+ones = np.ones(N)
+X = sm.add_constant(np.column_stack((X['LSTAT'], ones)))
+X = sm.add_constant(boston['LSTAT'])
+X['RM'] = boston['RM']
+multi_model = sm.OLS(y, X)
+multi_theta = multi_model.fit()
+multi_theta = multi_theta.params
 
 # Predict - Ordinary Least Squares (OLS) + Log Transformation.
 log_X = sm.add_constant(boston['LSTAT'])
@@ -63,20 +73,26 @@ log_theta = log_theta.params
 fig, ax = plt.subplots(figsize=(12,8))
 
 # Linear model.
-x = np.linspace(boston.min()['LSTAT'], boston.max()['LSTAT'], N)
+x = np.linspace(boston.min()['MEDV'], boston.max()['MEDV'], N)
 f = theta[0] + (theta[1] * x)
 prediction = lambda x: theta[0] + (theta[1] * x)
 
+# Linear model - Multivariate: LSTAT + RM.
+x = np.linspace(boston.min()['LSTAT'], boston.max()['LSTAT'], N)
+y = np.linspace(boston.min()['RM'], boston.max()['RM'], N)
+multi_f = multi_theta[0] + (multi_theta[1] * x) + (multi_theta[2] * y)
+multi_prediction = lambda x, y: multi_theta[0] + (multi_theta[1] * x) + (multi_theta[2] * y)
+
 # Linear model + Log Transformation.
-log_x = np.linspace(boston.min()['LSTAT'], boston.max()['LSTAT'], N)
-log_f = np.exp(log_theta[0] + (log_theta[1] * log_x))
-log_prediction = lambda log_x : np.exp(log_theta[0] + (log_theta[1] * log_x))
+x = np.linspace(boston.min()['MEDV'], boston.max()['MEDV'], N)
+log_f = np.exp(log_theta[0] + (log_theta[1] * x))
+log_prediction = lambda x : np.exp(log_theta[0] + (log_theta[1] * x))
 
 # Plot linear model.
 ax.plot(x, f, 'r', label='OLS Prediction', color='Black')
 
 # Plot linear model + Log Transformation.
-ax.plot(log_x, log_f, 'r', label='OLS Prediction + Log Transformation', color='Red')
+ax.plot(x, log_f, 'r', label='OLS Prediction + Log Transformation', color='Red')
 
 # Scatter
 ax.scatter(boston['MEDV'], boston['LSTAT'], label='Dataset', color='Cyan')
@@ -89,12 +105,15 @@ ax.set_title("MEDV vs LSTAT")
 
 # We need values without the one column for calculating the errors.
 X = boston['LSTAT']
+X1 = boston['RM']
+y = dataset.target
 
 # Calculate and print errors.
 print "RSME - Root Square Mean Error"
-rms = sqrt(mean_squared_error(boston['LSTAT'], f))
-log_rms = sqrt(mean_squared_error(boston['LSTAT'], log_f))
-print "Linear model:", rms, "Log model:", log_rms
+rms = sqrt(mean_squared_error(boston['MEDV'], f))
+log_rms = sqrt(mean_squared_error(boston['MEDV'], log_f))
+multi_rsme = sqrt(mean_squared_error(boston['MEDV'], multi_f))
+print "Linear model:", rms, "Log model:", log_rms, "Multivariate model:", multi_rsme
 print "\n"
 
 print "SSE - Error Sum of Squares or Residual Sum of Squares"
@@ -103,15 +122,15 @@ print "SSE - Error Sum of Squares or Residual Sum of Squares"
 # linear relationship.
 SSE = np.sum(np.square(np.subtract(y, prediction(X))))
 log_SSE = np.sum(np.square(np.subtract(y, log_prediction(X))))
-print "Linear model:", SSE, "Log model:", log_SSE
+multi_SSE = np.sum(np.square(np.subtract(y, multi_prediction(X, X1))))
+print "Linear model:", SSE, "Log model:", log_SSE, "Multivariate model:", multi_SSE
 print "\n"
 
 print "SST - Total Sum of Squares"
 # A quantitative measure of the total amount of variation in observed y
 # values is given by the total sum of squares.
 SST = np.sum(np.square(np.subtract(y, np.mean(y))))
-log_SST = np.sum(np.square(np.subtract(log_y, np.mean(y))))
-print "Linear model:", SST, "Log model:", log_SST
+print "All models:", SST
 print "\n"
 
 
@@ -119,6 +138,7 @@ print "R^2 - Coefficient of determination"
 # It is interpreted as the proportion of observed y variation that can be
 # explained by the simple linear regression model (attributed to an approximate
 # linear relationship between y and x).
-print "Linear model:", (1 - SSE/SST), "Log model:", (1 - log_SSE/log_SST)
+print "Linear model:", (1 - SSE/SST), "Log model:", (1 - log_SSE/SST),
+print "Multivariate model:", (1 - multi_SSE/SST)
 
 plt.show()
